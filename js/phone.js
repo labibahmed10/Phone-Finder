1eddc1b9c35878f4ad79abaeda813a63c7640a01
labibahmed10 <labib.ahmed.372@gmail.com>
2022-02-28 20:56:09 +0600

    updating

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,12 @@
-console.log("check");
+const findPhone = () => {
+  const inputValue = document.getElementById("input").value.toLowerCase();
+  document.getElementById("input").value = "";
+
+  fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
+    .then((res) => res.json())
+    .then((data) => showPhones(data.data));
+};
+
+const showPhones = (data) => {
+  data.forEach((phone) => console.log(phone));
+};
