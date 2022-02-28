bf3bfdf8fe88a00cc41b2fc31d6deeac6a9e7b9a
labibahmed10 <labib.ahmed.372@gmail.com>
2022-02-28 21:19:01 +0600

    updating

--- js/phone.js
+++ js/phone.js
@@ -8,5 +8,31 @@ const findPhone = () => {
 };
 
 const showPhones = (data) => {
-  data.forEach((phone) => console.log(phone));
+  data.slice(0, 20).forEach((phone) => {
+    const showCard = document.getElementById("cardshow");
+    console.log(phone);
+    // const phones = phone;
+    // console.log(phones.slice(0, 20));
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+    <div class="card h-100">
+    <img src="${phone.image}" class="card-img-top img-fluid" alt="..." />
+      <div class="card-body">
+        <h2 class="card-title">${phone.brand}</h2>
+        <div class="d-flex justify-content-between align-center">
+        <h4>${phone.phone_name}</h4>
+        <button class="btn btn-warning" onclick="showDetails('${phone.slug}')">Explore</button>
+        </div>
+      </div>
+    </div>
+    `;
+    showCard.appendChild(div);
+  });
 };
+
+
+
+const showDetails = (slug) =>{
+  
+}
