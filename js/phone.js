accafaaaefbf678edfb9515b4f43f1b04198d429
labibahmed10 <labib.ahmed.372@gmail.com>
2022-03-02 16:43:58 +0600

    updating

--- js/phone.js
+++ js/phone.js
@@ -50,10 +50,10 @@ const showPhones = (data) => {
     <div class="card h-100">
     <img src="${phone.image}" class="card-img-top img-fluid p-3" alt="..." />
       <div class="card-body">
-        <h2 class="card-title">${phone.brand}</h2>
-        <div class="d-flex justify-content-between align-center">
+        <h2 class="card-title brand">${phone.brand}</h2>
+        <div class="d-flex justify-content-between align-center brand">
         <h4>${phone.phone_name}</h4>
-        <button class="btn btn-warning" onclick="getId('${phone.slug}')">Explore</button>
+        <button class="btn btn-success" onclick="getId('${phone.slug}')">Explore</button>
         </div>
       </div>
     </div>
@@ -84,25 +84,35 @@ const showDetails = (details) => {
   phoneDetails.textContent = "";
   const div = document.createElement("div");
   div.classList.add("row");
-  console.log(details.others);
+  console.log(details);
   // checking if others are available
   if (details.others === undefined) {
     document.getElementById("phoneDetails").classList.remove("d-none");
     div.innerHTML = `
     <div class="col-md-12 mx-auto text-center">
       <img src="${details.image}" class="img-fluid w-75 rounded-start" alt="..." />
-      <h2 class="text-center mt-4">${details.name}</h2>
+      <h2 class="text-center mt-4 brand">${details.name}</h2>
     </div>
     <div class="col-md-12">
       <div class="card-body">
-        <h1 class="card-title fs-1 fw-bold">${details.brand}</h1>
+        <h1 class="card-title fs-1 fw-bold brand">${details.brand}</h1>
         <p class="text-muted fs-3 mb-0">${details.releaseDate ? details.releaseDate : "Coming Soon!"}</p>
         <h5 class="text-center display-5 fw-normal">Main Features</h5>
-        <p class="mb-0"><span class="fw-bold fs-4">Chipset:</span> ${details.mainFeatures.chipSet}</p>
-        <p class="mb-0"><span class="fw-bold fs-4">Display:</span> ${details.mainFeatures.displaySize}</p>
-        <p class="mb-0"><span class="fw-bold fs-4">Memory: </span>${details.mainFeatures.memory}</p>
-        <p class="mb-0"><span class="fw-bold fs-4">Storage:</span> ${details.mainFeatures.storage}</p>
-        <p class="mb-0"><span class="fw-bold fs-4">Sensors:</span> ${details.mainFeatures.sensors}</p>
+        <p class="mb-0 feature-text"><span class="fw-bold fs-4">Chipset:</span> ${
+          details.mainFeatures.chipSet
+        }</p>
+        <p class="mb-0 feature-text"><span class="fw-bold fs-4">Display:</span> ${
+          details.mainFeatures.displaySize
+        }</p>
+        <p class="mb-0 feature-text"><span class="fw-bold fs-4">Memory: </span>${
+          details.mainFeatures.memory
+        }</p>
+        <p class="mb-0 feature-text"><span class="fw-bold fs-4">Storage:</span> ${
+          details.mainFeatures.storage
+        }</p>
+        <p class="mb-0 feature-text"><span class="fw-bold fs-4">Sensors:</span> ${
+          details.mainFeatures.sensors
+        }</p>
       </div>
     </div>
         `;
@@ -113,25 +123,37 @@ const showDetails = (details) => {
     div.innerHTML = `
       <div class="col-md-12 mx-auto text-center">
         <img src="${details.image}" class="img-fluid w-75 rounded-start" alt="..." />
-        <h2 class="text-center mt-4">${details.name}</h2>
+        <h2 class="text-center mt-4 brand">${details.name}</h2>
       </div>
       <div class="col-md-12">
         <div class="card-body">
-          <h1 class="card-title fs-1 fw-bold">${details.brand}</h1>
+          <h1 class="card-title fs-1 fw-bold brand">${details.brand}</h1>
           <p class="text-muted fs-3 mb-0">${details.releaseDate ? details.releaseDate : "Coming Soon!"}</p>
-          <h5 class="text-center display-5 fw-normal">Main Features</h5>
-          <p class="mb-0"><span class="fw-bold fs-4">Chipset:</span> ${details.mainFeatures.chipSet}</p>
-          <p class="mb-0"><span class="fw-bold fs-4">Display:</span> ${details.mainFeatures.displaySize}</p>
-          <p class="mb-0"><span class="fw-bold fs-4">Memory: </span>${details.mainFeatures.memory}</p>
-          <p class="mb-0"><span class="fw-bold fs-4">Storage:</span> ${details.mainFeatures.storage}</p>
-          <p class="mb-0"><span class="fw-bold fs-4">Sensors:</span> ${details.mainFeatures.sensors}</p>
-          <h5 class="text-center display-5 fw-normal">Other Features</h5>
-          <p class="mb-0"><span class="fw-bold fs-4">Sensors:</span> ${details.others.Bluetooth}</p>
-          <p class="mb-0"><span class="fw-bold fs-4">GPS:</span> ${details.others.GPS}</p>
-          <p class="mb-0"><span class="fw-bold fs-4">NFC:</span> ${details.others.NFC}</p>
-          <p class="mb-0"><span class="fw-bold fs-4">Radio:</span> ${details.others.Radio}</p>
-          <p class="mb-0"><span class="fw-bold fs-4">USB:</span> ${details.others.USB}</p>
-          <p class="mb-0"><span class="fw-bold fs-4">WLAN:</span> ${details.others.WLAN}</p>
+          <h5 class="text-center display-5 fw-normal features">Main Features</h5>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">Chipset:</span> ${
+            details.mainFeatures.chipSet
+          }</p>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">Display:</span> ${
+            details.mainFeatures.displaySize
+          }</p>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">Memory: </span>${
+            details.mainFeatures.memory
+          }</p>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">Storage:</span> ${
+            details.mainFeatures.storage
+          }</p>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">Sensors:</span> ${
+            details.mainFeatures.sensors
+          }</p>
+          <h5 class="text-center display-5 fw-normal features">Other Features</h5>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">Bluetooth:</span> ${
+            details.others.Bluetooth
+          }</p>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">GPS:</span> ${details.others.GPS}</p>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">NFC:</span> ${details.others.NFC}</p>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">Radio:</span> ${details.others.Radio}</p>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">USB:</span> ${details.others.USB}</p>
+          <p class="mb-0 feature-text"><span class="fw-bold fs-4">WLAN:</span> ${details.others.WLAN}</p>
       </div>
       </div>
 `;
